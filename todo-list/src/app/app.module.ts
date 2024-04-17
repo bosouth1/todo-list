b1f44b0ebed8ff428019fd22bcb6ad06f06f78dc
bosouth <82606856+bosouth@users.noreply.github.com>
2024-04-17 13:09:06 -0500

    change file structure

--- todo-list/src/app/app.module.ts
+++ todo-list/src/app/app.module.ts
@@ -1,24 +1,20 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms'
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { TodoListComponent } from './todo-list/todo-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    DashboardComponent,
-    TodoListComponent
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
